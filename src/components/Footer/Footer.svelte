6516f6cdd053d0b2051f500c950825dabc7263e8
taese0ng <taese0ng@naver.com>
2022-06-01 11:29:33 +0900

    instagram 연결

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,7 +1,11 @@
-<script lang="ts"></script>
+<script lang="ts">
+  const handleClickContainer = () => {
+    window.open("https://www.instagram.com/soou_kaka/?hl=ko");
+  };
+</script>
 
 <div class="container">
-  <div class="wrapper">
+  <div class="wrapper" on:click={handleClickContainer}>
     토글토글스튜디오 <div class="with">dev taese0_0ng</div>
   </div>
 </div>
